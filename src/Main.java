92d72fe870860ad15b63a68ddc327cc0fe81c267
Макс Апаков <maksapakov@gmail.com>
2022-09-02 11:49:03 +0300

    ver.home_2.0

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,119 @@
-import java.sql.Array;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        DynamicArray array = new DynamicArray<>();
-/*
-        int count = 0;
-        while (count < 50) {
-            System.out.println(array.length() + " length");
-            array.add("One");
-            System.out.println(array.get(count));
-            System.out.println(array.size());
-            count++;
+        DynamicArray<String> strings = new DynamicArray<>();
+
+        System.out.println("Тест get():");
+        strings.add("Один");
+        strings.add("Два");
+        strings.add("Три");
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
         }
-*/
-        array.add("one");
-        array.add("two");
-        array.add("three");
-        array.add("four");
-        array.add("five");
-        array.add("six");
-        array.add("seven");
-        array.add("eight");
-        array.remove(1);
+        System.out.println("------------------------");
+
+        System.out.println("Тест add(). Добавим строку 'Четыре':");
+        strings.add("Четыре");
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
+
+        System.out.println("Тест remove(2). Удалим строку 'Два':");
+        strings.remove(1);
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
+
+        System.out.println("Тест remove(3). Удалим строку 'Три':");
+        strings.remove(1);
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
+
+        System.out.println("Тест add(). Добавим строку 'Семь':");
+        strings.add("Семь");
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
+
+        System.out.println("Тест add(). Добавим строку 'Восемь':");
+        strings.add("Восемь");
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
+
+        System.out.println("Тест add(). Добавим строку 'Девять':");
+        strings.add("Девять");
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
+
+        System.out.println("Тест add(). Добавим строку 'Пять':");
+        strings.add("Пять");
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
+
+        System.out.println("Тест remove(5). Удалим строку 'Пять':");
+        strings.remove(5);
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
+
+
+        System.out.println("Тест add(). Добавим строку 'Шесть':");
+        strings.add("Шесть");
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
+
+
+        System.out.println("Тест выхода за индекс:");
+        strings.get(10);
+        //strings.remove(10);
+        for (int i = 0; i < strings.length(); i++) {
+            System.out.println(i + 1 + ". get(" + i + ") = " + strings.get(i));
+        }
+        System.out.println("------------------------");
     }
 
     public static class DynamicArray<T> {
-        private int index;
         private int size;
-        private int capacity = 10;
-        private T[] array;
-
-        public DynamicArray() {
-            this.array = (T[]) new Object[capacity];
-        }
-
+        //        private int capacity = 1;
+        private T[] array = (T[]) new Object[1];
 
         public void add(T el) {
             if (size == array.length) {
-                array = Arrays.copyOf(array, array.length + 1);
+                array = Arrays.copyOf(array, array.length * 2);
             }
-            array[size] = el;
-            size++;
-            System.out.println(array[size-1]);
+            array[size++] = el;
         }
 
         public void remove(int index) {
-            this.index = index;
-            T[] arrayWithoutRemoveIndex = (T[]) new Object[capacity - 1];
-            for (int i = 0, j = 0; i < array.length; i++) {
-                if (i != index) {
-                    arrayWithoutRemoveIndex[j++] = array[i];
-                    System.out.println(array[i]);
-                }
+            final int newSize;
+            if ((newSize = size - 1) > index) {
+                System.arraycopy(array, index + 1, array, index, newSize - index);
+                array[size = newSize] = null;
+            } else {
+                array[size = newSize] = null;
             }
         }
 
         public T get(int index) {
-            try {
-                return array[index];
-            } catch (ArrayIndexOutOfBoundsException e) {
-                throw new ArrayIndexOutOfBoundsException(e.getMessage());
-            } finally {
-                return array[index];
+            if (index > array.length || index < 0 || array[index] == null) {
+                throw new ArrayIndexOutOfBoundsException();
             }
+            return array[index];
         }
 
         public int length() {
